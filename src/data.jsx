0d964a2cd9e25c861d2c4e6b947fdad222e00849
gregatgit <gregdd@outlook.com>
2020-10-13 22:18:01 +1000

    basic data

---
+++ src/data.jsx
@@ -0,0 +1,28 @@
+const data = {
+  items: {
+    'item-1': {id: 'item-1', content: 'Lorem ipsum dolor sit amet, '},
+    'item-2': {id: 'item-2', content: 'quis nostrud exercitation ullamco'},
+    'item-3': {id: 'item-3', content: 'Excepteur sint occaecat cupidatat '},
+    'item-4': {id: 'item-4', content: 'Duis aute irure dolor'},
+    'item-5': {id: 'item-5', content: 'sunt in culpa qui officia deserunt'},
+    'item-6': {id: 'item-6', content: 'consectetur adipiscing elit,'},
+    'item-7': {id: 'item-7', content: 'deserunt mollit anim id est laborum.'},
+    'item-8': {id: 'item-8', content: 'exercitation ullamco laboris'},
+    'item-9': {id: 'item-9', content: 'The passage experienced a surge in popularity '}
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      itemIds: ['item-1', 'item-2', 'item-3']
+    },
+    'column-2': {
+      id: 'column-2',
+      itemIds: ['item-4', 'item-5', 'item-6']
+    },
+    'column-3': {
+      id: 'column-3',
+      itemIds: ['item-7', 'item-8', 'item-9']
+    }
+  },
+  columnOrder: ['column-1', 'column-2', 'column-3']
+}
