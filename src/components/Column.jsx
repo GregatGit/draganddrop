975b97198e26546353348f2dd2d1cead5fc013fd
gregatgit <gregdd@outlook.com>
2020-10-14 14:15:09 +1000

    column can drag and drop

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Droppable, Draggable } from 'react-beautiful-dnd'
+
 import Item from './Item'
 
-const Column = ({ title, items }) => {
+const Column = ({ index, items, columnId }) => {
+  let count = index + 1
   return (
-    <div className="column">
-      <h2>{title}</h2>
-      <ul>{items && items.map((item) => <Item key={item.id} item={item.content} />)}</ul>
-    </div>
+    <Draggable draggableId={columnId} index={index}>
+      {(provided) => (
+        <div
+          className="column"
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          <h2 {...provided.dragHandleProps}>{count}</h2>
+          <ul>
+            {items &&
+              items.map((item) => <Item key={item.id} item={item.content} />)}
+          </ul>
+        </div>
+      )}
+    </Draggable>
   )
 }
 
--- src/components/DragAndDrop.jsx
+++ src/components/DragAndDrop.jsx
@@ -1,20 +1,66 @@
 import React, { useState } from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import Column from './Column'
 import { initData } from '../data'
 
 const DragAndDrop = () => {
   const [data, setData] = useState(initData)
+  const onDragEnd = (result) => {
+    console.log('drag ended: ', result)
+    const { destination, source, draggableId, type } = result
+    
+    if (!destination) return
+
+    if (destination.draggableId === source.droppableId &&
+        destination.index === source.index
+      ) return
+    
+    if (type === 'column'){
+      const newColumnOrder = [...data.columnOrder]
+      newColumnOrder.splice(source.index, 1)
+      newColumnOrder.splice(destination.index, 0, draggableId)
+      const state = { ...data }
+      state.columnOrder = newColumnOrder
+      setData(state)
+      return
+    }
+
+
+  }
   return (
     <div>
       <h1>Drag and Drop</h1>
-      <div className="main">
-        {data && data.columnOrder.map((columnId, index) => {
-          const items = data.columns[columnId].itemIds.map(id => {
-            return data.items[id]
-          })
-          return <Column key={columnId} items={items} index={index} />
-        })}
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable
+          droppableId="all-columns"
+          direction="horizontal"
+          type="column"
+        >
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="main"
+            >
+              {data &&
+                data.columnOrder.map((columnId, index) => {
+                  const items = data.columns[columnId].itemIds.map((id) => {
+                    return data.items[id]
+                  })
+                  return (
+                    <Column
+                      key={columnId}
+                      columnId={columnId}
+                      items={items}
+                      index={index}
+                    />
+                  )
+                })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
